a758808f71f0d773be60a2510cce5e7b0b74095a
凌辰亦梦 <xylingchen@yeah.net>
2021-03-10 17:46:00 +0800

    修改主页数据来源，默认从接口中获取

--- src/index.tsx
+++ src/index.tsx
@@ -6,6 +6,9 @@ import 'antd/dist/antd.css';
 import './i18n/configs';
 import { Provider } from 'react-redux';
 import store from './redux/store';
+import axios from 'axios';
+
+axios.defaults.headers['x-icode'] = '363A095858EF4B1D';
 
 ReactDOM.render(
   <React.StrictMode>
